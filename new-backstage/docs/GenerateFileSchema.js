c0310453028cbf0dae4bc8dbc9e30df14e3ea969
Mariane Previde <mariane.previde@gmail.com>
2021-04-08 10:21:45 -0300

    .

--- new-backstage/docs/GenerateFileSchema.js
+++ new-backstage/docs/GenerateFileSchema.js
@@ -2,8 +2,3 @@ const { writeFileSync } = require('fs');
 const { typeDefs } = require('../../graphql/Schema');
 
 writeFileSync(`${__dirname}/schemaDoc.graphql`, (typeDefs));
-
-// npm install -g @2fd/graphdoc
-
-// node ./src/docs/GenerateFileSchema.js
-// graphdoc -s ./src/docs/schemaDoc.graphql -o ./${VERSION} -f
